f6ead179295b540e29dd9ef3ddaf5985b5d9b880
Ezekiel Alera <aleraemmanuel@gmail.com>
2026-08-19 11:16:31 +0800

    Initial Commit: Fitness App finished (need to add more features)

--- src/App.jsx
+++ src/App.jsx
@@ -52,15 +52,23 @@ function App() {
   const [filter, setFilter] = useState("All");
 
   function handleAddWorkout() {
-    if (!date || !exercise || !sets || !reps || !weight) {
-      alert("Please fill in all fields");
+    const trimmedExercise = exercise.trim();
+
+    if (
+      !date ||
+      !trimmedExercise ||
+      Number(sets) <= 0 ||
+      Number(reps) <= 0 ||
+      Number(weight) <= 0
+    ) {
+      alert("Please fill in all fields with valid positive numbers.");
       return;
     }
 
     const newWorkout = {
       id: nextId++,
       date,
-      exercise,
+      exercise: trimmedExercise,
       muscleGroup,
       sets: Number(sets),
       reps: Number(reps),
@@ -84,16 +92,20 @@ function App() {
     filter === "All"
       ? workouts
       : workouts.filter((w) => w.muscleGroup === filter);
-  
+
+  const sortedWorkouts = [...filteredWorkouts].sort(
+    (a, b) => new Date(b.date) - new Date(a.date),
+  );
+
   const chartData = [...filteredWorkouts]
-    .sort((a,b) => new Date(a.date) - new Date(b.date))
+    .sort((a, b) => new Date(a.date) - new Date(b.date))
     .map((w) => ({
       date: w.date,
       volume: w.sets * w.reps * w.weight,
     }));
 
   return (
-    <div className="min-h-screen w-full bg-slate-200 py-10 px-4">
+    <div className="min-h-screen w-full overflow-x-hidden bg-slate-200 py-10 px-4">
       <div className="max-w-3xl mx-auto flex flex-col gap-6">
         <h1 className="font-mono text-2xl font-bold text-slate-800">
           Fitness Progress Dashboard
@@ -131,6 +143,7 @@ function App() {
             </select>
             <input
               type="number"
+              min="1"
               placeholder="Sets"
               value={sets}
               onChange={(e) => setSets(e.target.value)}
@@ -138,6 +151,7 @@ function App() {
             />
             <input
               type="number"
+              min="1"
               placeholder="Reps"
               value={reps}
               onChange={(e) => setReps(e.target.value)}
@@ -145,6 +159,7 @@ function App() {
             />
             <input
               type="number"
+              min="1"
               placeholder="Weight (kg)"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
@@ -184,20 +199,22 @@ function App() {
           </h2>
 
           {chartData.length === 0 ? (
-            <p className="text-slate-400 text-sm">No Data for this filter yet</p>
+            <p className="text-slate-400 text-sm">
+              No Data for this filter yet
+            </p>
           ) : (
             <ResponsiveContainer width="100%" height={250}>
               <LineChart data={chartData}>
-                <CartesianGrid strokeDasharray="3 3" stroke="#e2e8fo"/>
-                <XAxis dataKey="date" tick={{fontSize: 12}}/>
-                <YAxis tick={{ fontSize: 12 }}/>
-                <Tooltip/>
+                <CartesianGrid strokeDasharray="3 3" stroke="#e2e8f0" />
+                <XAxis dataKey="date" tick={{ fontSize: 12 }} />
+                <YAxis tick={{ fontSize: 12 }} />
+                <Tooltip />
                 <Line
                   type="monotone"
                   dataKey="volume"
                   stroke="#2563eb"
                   strokeWidth={2}
-                  dot={{r:3}}
+                  dot={{ r: 3 }}
                 />
               </LineChart>
             </ResponsiveContainer>
@@ -209,44 +226,46 @@ function App() {
             Workout History
           </h2>
 
-          {filteredWorkouts.length === 0 ? (
+          {sortedWorkouts.length === 0 ? (
             <p className=" text-slate-400 text-sm">No Workouts logged yet.</p>
           ) : (
-            <table className="w-full text-sm text-left">
-              <thead>
-                <tr className="border-b border-slate-200 text-slate-500">
-                  <th className="py-2 pr-2">Date</th>
-                  <th className="py-2 pr-2">Exercise</th>
-                  <th className="py-2 pr-2">Group</th>
-                  <th className="py-2 pr-2">Sets</th>
-                  <th className="py-2 pr-2">Reps</th>
-                  <th className="py-2 pr-2">Weight</th>
-                  <th className="py-2 pr-2"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredWorkouts.map((w) => (
-                  <tr key={w.id} className="border-b border-slate-100">
-                    <td className="py-2 pr-2 text-slate-700">{w.date}</td>
-                    <td className="py-2 pr-2 text-slate-700">{w.exercise}</td>
-                    <td className="py-2 pr-2 text-slate-700">
-                      {w.muscleGroup}
-                    </td>
-                    <td className="py-2 pr-2 text-slate-700">{w.sets}</td>
-                    <td className="py-2 pr-2 text-slate-700">{w.reps}</td>
-                    <td className="py-2 pr-2 text-slate-700">{w.weight}</td>
-                    <td className="py-2 pr-2">
-                      <button
-                        onClick={() => handleDeleteWorkout(w.id)}
-                        className="text-red-500 hover:text-red-700"
-                      >
-                        Delete
-                      </button>
-                    </td>
+            <div className="overflow-x-auto">
+              <table className="w-full text-sm text-left">
+                <thead>
+                  <tr className="border-b border-slate-200 text-slate-500">
+                    <th className="py-2 pr-2">Date</th>
+                    <th className="py-2 pr-2">Exercise</th>
+                    <th className="py-2 pr-2">Group</th>
+                    <th className="py-2 pr-2">Sets</th>
+                    <th className="py-2 pr-2">Reps</th>
+                    <th className="py-2 pr-2">Weight</th>
+                    <th className="py-2 pr-2"></th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {sortedWorkouts.map((w) => (
+                    <tr key={w.id} className="border-b border-slate-100">
+                      <td className="py-2 pr-2 text-slate-700">{w.date}</td>
+                      <td className="py-2 pr-2 text-slate-700">{w.exercise}</td>
+                      <td className="py-2 pr-2 text-slate-700">
+                        {w.muscleGroup}
+                      </td>
+                      <td className="py-2 pr-2 text-slate-700">{w.sets}</td>
+                      <td className="py-2 pr-2 text-slate-700">{w.reps}</td>
+                      <td className="py-2 pr-2 text-slate-700">{w.weight}</td>
+                      <td className="py-2 pr-2">
+                        <button
+                          onClick={() => handleDeleteWorkout(w.id)}
+                          className="text-red-500 hover:text-red-700"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           )}
         </div>
       </div>
